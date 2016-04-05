38b687688e03974962ef3580a45261d1622bef81
StuartLC <nonamebrande@gmail.com>
2016-04-05 08:43:32 +0200

    http://stackoverflow.com/questions/36353982/

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using AutoMapper;
+
+namespace ConsoleApplication1
+{
+    public class FamilyModel
+    {
+        public int FamilyID { get; set; }
+        public string FamilyName { get; set; }
+    }
+
+    public class SaveKitViewModel
+    {
+        public int FamilyID { get; set; }
+        public string FamilyName { get; set; }
+        public int UserId { get; set; }
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mapper = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<FamilyModel, SaveKitViewModel>();
+            }).CreateMapper();
+
+            var existingSaveKitViewModel = new SaveKitViewModel
+            {
+                UserId = 1234
+            };
+            var familyModel = new FamilyModel
+            {
+                FamilyID = 9876,
+                FamilyName = "Bloggs"
+            };
+
+            mapper.Map(familyModel, existingSaveKitViewModel);
+
+            Console.WriteLine(existingSaveKitViewModel.FamilyID);
+            Console.WriteLine(existingSaveKitViewModel.FamilyName);
+            Console.WriteLine(existingSaveKitViewModel.UserId);
+
+            // Obviously in a production system, you would bootstrap the Automapper MapperConfiguration just once, 
+            // and then use the global Mapper.Map(familyModel, existingSaveKitViewModel); elsewhere when needed
+        }
+    }
+}
